0049887862f6a1a595e151a32ca72023fcf93b3a
Olu Adegun <olu.adegun@maplequad.com>
2019-06-27 10:23:29 +0100

    added swagger url

--- src/promedic/promedic/urls.py
+++ src/promedic/promedic/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.views import serve
+from rest_framework_swagger.views import get_swagger_view
 
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^api/core/', include('core.urls')),
     url(r'^api/medic/', include('medic.urls')),
