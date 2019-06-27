28816cf7a0168309364e547e2df076b3c5db9769
Olu Adegun <olu.adegun@maplequad.com>
2019-06-27 10:25:32 +0100

    fixed url ish

--- src/promedic/promedic/urls.py
+++ src/promedic/promedic/urls.py
@@ -24,7 +24,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
-    url(r'^$', schema_view),
+    url(r'^/', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^api/core/', include('core.urls')),
     url(r'^api/medic/', include('medic.urls')),
